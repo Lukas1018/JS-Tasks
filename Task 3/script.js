1cba4b42e3058e79c6923d0322ce67f37bfe8e9a
Lukas1018 <lukasvelzys@gmail.com>
2022-09-29 00:04:22 +0300

    completed task 3

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,41 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", showUsers);
+
+function showUsers() {
+  document.querySelector("#message").remove();
+  const createUserCard = (userData) => {
+    userData.forEach((user) => {
+      const userCard = document.createElement("div");
+      const avatar = document.createElement("img");
+      const login = document.createElement("h3");
+
+      userCard.setAttribute("id", "user-card");
+      avatar.setAttribute("id", "avatar");
+      login.setAttribute("id", "login");
+      console.log(userCard);
+      login.innerText = user.login;
+      avatar.src = user.avatar_url;
+
+      userCard.append(avatar, login);
+      document.querySelector("#output").append(userCard);
+    });
+  };
+  const getUsers = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+
+      createUserCard(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getUsers(ENDPOINT);
+}
